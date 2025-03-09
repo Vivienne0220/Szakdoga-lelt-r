5949fa3b9b4b099f9242e8ee790f4c4fd30338c3
Vivienne0220 <viviszenczi@gmail.com>
2025-03-09 22:37:24 +0100

    Add files via upload
    Kategóriák létrehozása, és azoknak kiszámolása

---
+++ Szakdoga-lelt-r-main/proba_javitott_again/Termekek.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace proba
+{
+    [Serializable]
+    public enum KategoriaType
+    {
+        Sud = 1,
+        Flasa = 2,
+        Pleh = 3,
+        Sklenené = 4,
+        Nič = 5
+    }
+
+    public static class KategoriaHelper
+{
+    private static readonly Dictionary<KategoriaType, decimal> KategoriaErtekek = new Dictionary<KategoriaType, decimal>
+    {
+        { KategoriaType.Sud, 0.00m },
+        { KategoriaType.Flasa, 0.00m },
+        { KategoriaType.Pleh, 0.15m },
+        { KategoriaType.Sklenené, 0.13m },
+        { KategoriaType.Nič, 0.00m }
+    };
+
+    public static decimal GetFixErtek(KategoriaType kategoria)
+    {
+        return KategoriaErtekek.ContainsKey(kategoria) ? KategoriaErtekek[kategoria] : 0.00m;
+    }
+}
+
+    public class Termekek
+    {
+   
+
+        [BsonId, BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+        public string ProductId { get; set; }
+
+        [BsonElement("product_code"), BsonRepresentation(BsonType.String)]
+        public string ProductCode { get; set; }
+
+        [BsonElement("product_name"), BsonRepresentation(BsonType.String)]
+        public string ProductName { get; set; }
+
+        [BsonElement("price"), BsonRepresentation(BsonType.Decimal128)]
+        public decimal Price { get; set; }
+
+        [BsonElement("zosPred"), BsonRepresentation(BsonType.Decimal128)]
+        public decimal ZosPred { get; set; }
+
+        [BsonElement("prijem"), BsonRepresentation(BsonType.Decimal128)]
+        public decimal Prijem { get; set; }
+
+        [BsonElement("uzavZos"), BsonRepresentation(BsonType.Decimal128)]
+        public decimal UzavZos { get; set; }
+
+        [BsonElement("kategoria"), BsonRepresentation(BsonType.String)]
+        public KategoriaType Kategoria { get; set; }
+    }
+}
